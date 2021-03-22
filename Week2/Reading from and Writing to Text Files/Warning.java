82a58d96b7a8814edfffee6de4159849224aa2ad
Herlana <hrlana1312@gmail.com>
2021-03-22 12:25:26 +0700

    menambahkan file students.dat

--- Week2/Reading from and Writing to Text Files/Warning.java
+++ Week2/Reading from and Writing to Text Files/Warning.java
@@ -26,23 +26,39 @@ public class Warning
         double gpa;        // grade point (quality point) average  
         String line, name, inputName = "students.dat";  
         String outputName = "warning.dat"; 
-
+        
+        Scanner scan;
+        PrintWriter outFile;
         try  
         {  
-            // Set up scanner to input file  
-            // Set up the output file stream  
-            // Print a header to the output file  
-            outFile.println ();  
-            outFile.println ("Students on Academic Warning");   
-            outFile.println ();  
+          // Set up scanner to input file  
+          scan = new Scanner(new FileInputStream(inputName));
+          // Set up the output file stream  
+          outFile = new PrintWriter(new FileWriter(outputName));  
+          // Print a header to the output file  
+            outFile.println();  
+            outFile.println("Students on Academic Warning");   
+            outFile.println();  
             // Process the input file, one token at a time    
-            while ()  
+            while (scan.hasNextLine())  
             {  
-                // Get the credit hours and quality points and   
-                // determine if the student is on warning. If so,   
-                // write the student data to the output file.  
+                // Get the credit hours and quality points and
+                name = scan.next();
+                creditHrs = scan.nextInt();
+                qualityPts = scan.nextDouble();
+                gpa = qualityPts/creditHrs;
+                // determine if the student is on warning. If so,
+                if ((gpa < 1.5 && creditHrs < 30) || (gpa < 1.75 && creditHrs < 60) 
+                        || (gpa < 2.0 && creditHrs >= 60))
+                {
+                    // write the student data to the output file.
+                    outFile.print(name + " ");
+                    outFile.print(creditHrs + " ");
+                    outFile.print(gpa);
+                }                         
             }  
-            // Close output file  
+            // Close output file
+            outFile.close();
         }  
         catch (FileNotFoundException exception)  
         {  
