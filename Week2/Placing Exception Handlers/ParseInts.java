1620eb33b85f5e2724c38bd1485b440ded923694
Herlana <hrlana1312@gmail.com>
2021-03-23 22:03:58 +0700

    Add try catch before the while

--- Week2/Placing Exception Handlers/ParseInts.java
+++ Week2/Placing Exception Handlers/ParseInts.java
@@ -23,10 +23,13 @@ public class ParseInts
         System.out.println("Enter a line of text");
         Scanner scanLine = new Scanner(scan.nextLine());
         
-        while (scanLine.hasNext())
-        {            
-			val = Integer.parseInt(scanLine.next());
-			sum += val;            
+		try{
+			while (scanLine.hasNext())
+			{            
+				val = Integer.parseInt(scanLine.next());
+				sum += val;            
+			}
+		}catch(NumberFormatException n){        
         }
         
         System.out.println("The sum of the integers on this line is " + sum);
