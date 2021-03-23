7ae2240c897e2ab4fdc14dcc37ebb0e612632519
Herlana <hrlana1312@gmail.com>
2021-03-23 22:05:12 +0700

    move the try and catch inside the while

--- Week2/Placing Exception Handlers/ParseInts.java
+++ Week2/Placing Exception Handlers/ParseInts.java
@@ -23,13 +23,13 @@ public class ParseInts
         System.out.println("Enter a line of text");
         Scanner scanLine = new Scanner(scan.nextLine());
         
-		try{
-			while (scanLine.hasNext())
-			{            
-				val = Integer.parseInt(scanLine.next());
-				sum += val;            
-			}
-		}catch(NumberFormatException n){        
+        while (scanLine.hasNext())
+        {
+            try{
+                val = Integer.parseInt(scanLine.next());
+                sum += val;
+            }catch(NumberFormatException n){        
+            }
         }
         
         System.out.println("The sum of the integers on this line is " + sum);
